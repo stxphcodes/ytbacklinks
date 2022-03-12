45f9493b6aea4aa610744f953ddc643966a1e039
szh2425 <huangs4@chop.edu>
2022-03-12 16:33:46 -0500

    Get jennims recent 10 video descriptions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import type {NextPage} from 'next';
+import { getUploadPlaylistId, getVideoDescriptions } from '../utils/getVideoDescriptions';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className="text-center my-24 font-black tracking-tight text-6xl">
-      Our homepage
-    </h1>
-  );
+type Props = {
+  descriptions: string[];
 };
 
-export default Home;
+export async function getServerSideProps() {
+  const uploadPlaylistId = await getUploadPlaylistId();
+
+  const descriptions = await getVideoDescriptions(uploadPlaylistId);
+
+  console.log("these are descriptions")
+  console.log(descriptions)
+
+  return {
+    props: {descriptions},
+  };
+}
+
+export default function Index({descriptions}: Props) {
+  return (
+    <div className="p-12">
+      <h1 className="text-center my-24 font-black tracking-tight text-6xl">
+        Links in the Description
+      </h1>
+      <div className="columns-3 gap-3">
+        {descriptions.map(description => {
+          return <div className="p-9 my-3 border-2 whitespace-pre-wrap">{description}</div>;
+        })}
+      </div>
+    </div>
+  );
+}
