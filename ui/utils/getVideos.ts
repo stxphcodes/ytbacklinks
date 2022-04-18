3315c04e5b098e30de1f370fee39ae7e867dea21
stxphcodes <szh2425@gmail.com>
2022-04-18 19:17:20 -0400

    Sort videos client side

--- ui/utils/getVideos.ts
+++ ui/utils/getVideos.ts
@@ -2,35 +2,30 @@ import { Link, VideoUI } from './types';
 
 const FIREBASE_URL = 'https://backlinks-81c44-default-rtdb.firebaseio.com/';
 
-export type VideoResponse = {
-  [key: string]: VideoUI;
-};
-
-export async function getVideos(
-  channelId: string
-): Promise<VideoResponse | null> {
+export async function getVideos(channelId: string): Promise<VideoUI[] | null> {
   let firebase = new URL(FIREBASE_URL);
   firebase.pathname = `videosByChannels/${channelId}.json`;
 
   let response = await fetch(firebase.toString());
   if (!response.ok) {
+    console.log(response);
     return null;
   }
 
-  let r: VideoResponse = await response.json();
+  let data: {[videoId: string]: VideoUI} = await response.json();
+
+  let sorted = Object.values(data).sort((videoA, videoB) =>
+    videoB.PublishedAt.localeCompare(videoA.PublishedAt)
+  );
 
   await Promise.all(
-    Object.keys(r).map(async videoId => {
-      r[videoId].Links = await getLinks(channelId, videoId);
+    sorted.map(async (video, index) => {
+      sorted[index].Links = await getLinks(channelId, video.Id);
     })
   );
 
-  return r;
+  return sorted;
 }
-
-export type LinkResponse = {
-  [key: string]: Link;
-};
 
 async function getLinks(channelId: string, videoId: string): Promise<Link[]> {
   let firebase = new URL(FIREBASE_URL);
@@ -40,7 +35,7 @@ async function getLinks(channelId: string, videoId: string): Promise<Link[]> {
 
   let links: Link[] = [];
   if (response.ok) {
-    let r: LinkResponse | null = await response.json();
+    let r: {[linkId: string]: Link} = await response.json();
 
     if (r === null) {
       return links;
