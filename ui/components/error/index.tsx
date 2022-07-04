9dfac0ee47971e024a4387be33d29f177794c05e
stxphcodes <szh2425@gmail.com>
2022-07-04 18:20:24 -0400

    Create error component

---
+++ ui/components/error/index.tsx
@@ -0,0 +1,18 @@
+import { ApiResponse } from '../../utils/types';
+
+type Props = {
+  response: ApiResponse;
+};
+
+export default function ErrorPage({response}: Props) {
+  return (
+    <div className="p-12">
+      <h1 className="text-center my-12 font-black tracking-tight text-6xl">
+        {response.Status} {response.StatusText} Error
+      </h1>
+      <h3 className="text-center my-4 font-black tracking-tight text-3xl">
+        {response.Message}
+      </h3>
+    </div>
+  );
+}
