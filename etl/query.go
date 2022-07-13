c43b35172ae4c7d624a43ea05b7739310dc27c86
stxphcodes <szh2425@gmail.com>
2022-07-13 09:31:25 -0400

    Query for videos in database

--- etl/query.go
+++ etl/query.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"firebase.google.com/go/db"
 )
 
 func queryLastUpdated(ctx context.Context, client *db.Client) (string, error) {
-	ref := client.NewRef("/lastUpdated")
+	ref := client.NewRef(LAST_UPDATED_REF)
 
 	// get last element of array
 	query := ref.OrderByKey().LimitToLast(1)
@@ -25,4 +26,26 @@ func queryLastUpdated(ctx context.Context, client *db.Client) (string, error) {
 	}
 
 	return date, nil
+}
+
+func queryVideosByChannelId(ctx context.Context, client *db.Client) (map[string]map[string]*Video, error) {
+	ref := client.NewRef(VIDEOS_BY_CHANNELS_REF)
+
+	var channelToVideos map[string]map[string]*Video
+	if err := ref.Get(ctx, &channelToVideos); err != nil {
+		return nil, err
+	}
+
+	return channelToVideos, nil
+}
+
+func queryChannelTitleById(ctx context.Context, client *db.Client, id string) (string, error) {
+	ref := client.NewRef(fmt.Sprintf("%s/%s", CHANNELS_REF, id))
+
+	var channel Channel
+	if err := ref.Get(ctx, &channel); err != nil {
+		return "", err
+	}
+
+	return channel.Title, nil
 }
