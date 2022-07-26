c508fe99eecdd0fe685837321743fc206efb4fec
stxphcodes <szh2425@gmail.com>
2022-07-26 15:40:50 -0400

    Add config to next env to create firestore client

---
+++ ui/utils/getFirestoreClient.ts
@@ -0,0 +1,38 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+import { TResponseWrapper } from './responseWrapper';
+
+export function getFirestoreClient(): TResponseWrapper {
+    if (!process.env.FIREBASE_CONFIG || process.env.FIREBASE_CONFIG == "") {
+        return {
+            Ok: false, 
+            Status: 500,
+            StatusText: 'Internal Server',
+            Message: 'FIREBASE_CONFIG env not set.',
+            RawMessage: 'In getFirestoreClient.',  
+        }
+    }
+
+    try {
+        let cfg = JSON.parse(process.env.FIREBASE_CONFIG)      
+        const app = initializeApp(cfg);
+        const firestore = getFirestore(app);
+
+        return {
+            Ok: true,
+            Status: 200, 
+            StatusText: 'Ok',
+            Message: firestore, 
+            RawMessage: firestore,
+        }
+    } catch (error: any) {
+        return {
+            Ok: false,
+            Status: 500, 
+            StatusText: 'Internal Server',
+            Message: error.message || error.Message || `Error initializing firestore client`,
+            RawMessage: error.cause || 'In getFirestoreClient'
+        }
+    }
+}
