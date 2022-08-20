ef2e6dc543b576009ca623b15a99f7be2c4dfd16
stxphcodes <szh2425@gmail.com>
2022-08-20 13:28:42 -0400

    Update design

--- ui/components/searchbar/index.tsx
+++ ui/components/searchbar/index.tsx
@@ -1,5 +1,3 @@
-
-
 type Props = {
   handleInputChange: any;
   handleSubmit: any;
@@ -14,7 +12,7 @@ export default function Searchbar({ handleInputChange, handleSubmit, inputValue}
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
           <svg
             aria-hidden="true"
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            className="w-5 h-5"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +27,7 @@ export default function Searchbar({ handleInputChange, handleSubmit, inputValue}
         <input
           type="text"
           id="simple-search"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="focus:outline-theme-yt-red rounded-lg pl-10 p-2.5 text-lg w-full"
           placeholder="Search"
           required
           onChange={handleInputChange}
@@ -38,7 +36,7 @@ export default function Searchbar({ handleInputChange, handleSubmit, inputValue}
       </div>
       <button
         type="submit"
-        className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="bg-theme-yt-red font-medium hover:bg-theme-yt-red-2 ml-2 py-2.5 px-4 text-md  rounded-lg text-white"
         onClick={handleSubmit}
       >
         Search
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-yt-red': '#e63b2f',
+        'theme-yt-red-1': '#e1625a',
+        'theme-yt-red-2': '#BF3026',
+        'theme-beige': '#fefdfb',
+        'theme-beige-1': '#F2F1F0',
+        'theme-beige-2': '#e3e2e0',
+      },
+    },
   },
   plugins: [],
-}
+};
