bd04bf682494a81e22df3bf42068b6da6b969a1d
stxphcodes <szh2425@gmail.com>
2022-08-20 13:33:43 -0400

    Use next/Link component

--- ui/components/navbar/index.js
+++ ui/components/navbar/index.js
@@ -1,11 +1,12 @@
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
     <nav class="bg-theme-beige flex space-x-8 p-4 shadow-sm sticky text-theme-yt-red top-0">
       <div> YT Backlinks</div>
       <div className="space-x-8">
-        <a href="/">Home</a>
-        <a href="">About</a>
-        <a href="">Contact Us</a>
+        <Link href="/">Home</Link>
+        <Link href="">About</Link>
       </div>
     </nav>
   );
