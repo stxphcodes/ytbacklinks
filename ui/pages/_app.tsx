ff4043fcab245bcfdeb1f1eb38491d1dcd8a9439
stxphcodes <szh2425@gmail.com>
2022-09-30 15:59:31 -0400

    Add icon and title to site

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,13 +1,19 @@
 import '../styles/base.css';
 
+import Head from 'next/head';
+
 import Banner from '../components/banner';
 import Navbar from '../components/navbar';
 
-import type {AppProps} from 'next/app';
+import type { AppProps } from "next/app";
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Youtube Backlinks</title>
+        <link rel="icon" type="image/x-icon" href="/static/favicon.png"></link>
+      </Head>
       <Navbar />
       <Banner />
       <Component {...pageProps} />
