66810682ca4bcd9e33c065c26df27655d9c5fcb7
stxphcodes <szh2425@gmail.com>
2022-09-30 11:23:47 -0400

    Address console warnings

--- ui/components/icons/chevron.tsx
+++ ui/components/icons/chevron.tsx
@@ -4,13 +4,13 @@ export function ChevronUpIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="w-4 h-4 md:w-6 md:h-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M4.5 15.75l7.5-7.5 7.5 7.5"
       />
     </svg>
@@ -23,13 +23,13 @@ export function ChevronDownIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="w-4 h-4 md:w-6 md:h-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M19.5 8.25l-7.5 7.5-7.5-7.5"
       />
     </svg>
