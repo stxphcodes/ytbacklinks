d7b44a636c54db8198bbfa44866ff65eb429c92b
stxphcodes <szh2425@gmail.com>
2022-10-01 11:07:06 -0400

    Make components mobile friendly

--- ui/components/toggle/index.tsx
+++ ui/components/toggle/index.tsx
@@ -3,8 +3,8 @@ export default function Toggle(props: {
   handleClick: any;
 }) {
   return (
-    <div className="flex items-center mt-4 bg-theme-yt-red w-fit p-2 rounded-full text-white">
-      <span className="mr-1 text-sm font-medium ">Links only</span>
+    <div className="flex items-center mt-4 bg-theme-yt-red w-fit p-2 rounded-full text-white text-xs sm:text-sm">
+      <span className="mr-1 font-medium ">Links only</span>
 
       <label className="inline-flex relative items-center  cursor-pointer -">
         <input
@@ -18,7 +18,7 @@ export default function Toggle(props: {
           className="w-11 h-5 bg-theme-beige-2 rounded-full peer peer-focus:ring-theme-yt-red peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0 after:left-[2px] after:bg-theme-yt-red after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-theme-beige-2"
         ></div>
       </label>
-      <span className="ml-1 text-sm font-medium">Full description box</span>
+      <span className="ml-1 font-medium">Full description box</span>
     </div>
   );
 }
